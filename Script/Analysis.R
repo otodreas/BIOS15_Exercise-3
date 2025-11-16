052e8597e2f7f69a4023a16be1856404dc24832b
otodreas <159846818+otodreas@users.noreply.github.com>
2025-11-16 21:08:27 +0100

    finished draft, created means file

--- Script/Analysis.R
+++ Script/Analysis.R
@@ -18,10 +18,10 @@ data <- as_tibble(df)
 data <- data %>% select(LarvalHost, MaternalHost, AdultWeight, GrowthRate)
 
 # Fit linear models and perform ANOVA
-m_aw <- lm(data$AdultWeight ~ data$LarvalHost * data$MaternalHost - 1)
 m_gr <- lm(data$GrowthRate ~ data$LarvalHost * data$MaternalHost - 1)
-anova_aw <- anova(m_aw)
+m_aw <- lm(data$AdultWeight ~ data$LarvalHost * data$MaternalHost - 1)
 anova_gr <- anova(m_gr)
+anova_aw <- anova(m_aw)
 
 # Save ANOVA output to csv
 write.csv(anova_aw, here("Output", "anova_aw.csv"))
@@ -30,21 +30,27 @@ write.csv(anova_gr, here("Output", "anova_gr.csv"))
 # Pivot data along responses to help with plotting facets
 data_p <- data %>%
   pivot_longer(
-    cols = AdultWeight:GrowthRate, names_to = "Response", values_to = "Value"
+    cols = GrowthRate:AdultWeight, names_to = "Response", values_to = "Value"
   )
 
+# Create dataframe data_summary to contain means of boxes in plot and save
+data_summary <- data_p %>%
+  group_by(LarvalHost, MaternalHost, Response) %>%
+  summarize(mean_response_value = mean(Value))
+write.csv(data_summary, here("Output", "data_summary.csv"))
+
 # Create custom labels for the plot facets
-response.labs <- c("Adult Weight (mg)", "Growth Rate (mg/day)")
-names(response.labs) <- c("AdultWeight", "GrowthRate")
+response.labs <- c("Growth Rate (mg/day)", "Adult Weight (mg)")
+names(response.labs) <- c("GrowthRate", "AdultWeight")
 
 # Plot and save
 data_p %>%
   ggplot(aes(
-    x = factor(LarvalHost, level = c("Berteroa", "Barbarea")), y = Value,
-    fill = MaternalHost
+    x = factor(MaternalHost, level = c("Barbarea", "Berteroa")), y = Value,
+    fill = LarvalHost
   )) +
   geom_boxplot() +
-  labs(x = "Larval Host", y= NULL, fill = "Maternal Host") +
+  labs(x = "Maternal Host", y = NULL, fill = "Larval Host") +
   facet_wrap(
     facets = vars(Response), nrow = 2,
     strip.position = "left",  # Move facet labels left to act as y-axis labels
@@ -52,6 +58,6 @@ data_p %>%
   ) +
   theme(
     strip.background = element_blank(),  # Clear background behind facet labels
-    strip.placement='outside'  # Place labels outside of axis ticks
+    strip.placement = "outside"  # Place labels outside of axis ticks
   )
-ggsave(here("Output", "Fig1.svg"), width = 1200, height = 1000, units = "px")
+ggsave(here("Output", "Fig1.svg"), width = 1100, height = 1100, units = "px")
